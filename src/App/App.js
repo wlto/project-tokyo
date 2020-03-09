86751c7ef86ecf8af4b80d6d74a2cf4ca5efdb3c
William To <towilliam03@gmail.com>
2020-03-09 17:36:17 -0400

    Drawing with canvas

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+// Libraries
+import { cover } from 'intrinsic-scale';
+
 // Components
 import FetchedImage from '../FetchedImage/FetchedImage';
 import NeumorphismButton from '../NeumorphismButton/NeumorphismButton';
@@ -12,8 +15,35 @@ require('dotenv').config();
 function App() {
   const unsplashAPIEndpoint = `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&query=tokyo`;
 
+  const colours = [
+    {
+      hexValue: '#000000',
+      percentage: '20%'
+    },
+    {
+      hexValue: '#914E72',
+      percentage: '11%'
+    },
+    {
+      hexValue: '#F15060',
+      percentage: '9%'
+    },
+    {
+      hexValue: '#3D5588',
+      percentage: '6%'
+    },
+    {
+      hexValue: '#00838A',
+      percentage: '4%'
+    }
+  ]
+
   // State
-  const [fetchedImage, setFetchedImage] = useState({});
+  const [fetchedImage, setFetchedImage] = useState({
+    url: '',
+    author: '',
+    unsplashUrl: ''
+  });
 
   const fetchImage = async () => {
     const randomIndex = parseInt(Math.random() * 10);
@@ -22,7 +52,7 @@ function App() {
     const fetchedResult = body.results[randomIndex];
 
     setFetchedImage({
-      url: fetchedResult.urls.regular,
+      url: fetchedResult.urls.full,
       author: fetchedResult.user.name,
       unsplashUrl: fetchedResult.links.html
     });
@@ -33,6 +63,30 @@ function App() {
     fetchImage();
   }, []);
 
+  useEffect(() => {
+    const image = new Image();
+
+    var canvas = document.getElementById('App-image-canvas');
+    var context = canvas.getContext('2d');
+    canvas.width = window.innerWidth * 80 / 100;
+    canvas.height = canvas.width;
+
+    image.setAttribute("src", fetchedImage.url);
+
+    // Credits: https://riptutorial.com/html5-canvas/example/19169/scaling-image-to-fit-or-fill-
+    image.addEventListener('load', function () {
+      var scale = Math.max(canvas.width / image.width, canvas.height / image.height);
+      // Get the top left position of the image
+      var x = (canvas.width / 2) - (image.width / 2) * scale;
+      var y = (canvas.height / 2) - (image.height / 2) * scale;
+      context.drawImage(image, x, y, image.width * scale, image.height * scale);
+    });
+
+    // context.drawImage(image, 0, 0);
+    // var myData = context.getImageData(0, 0, 800, 400).data;
+    // console.log(myData);
+  }, [fetchedImage]);
+
   const handleOnClick = (event) => {
     fetchImage();
   }
@@ -41,13 +95,16 @@ function App() {
     <div className="App">
       <h1 className="App-title">tokyo</h1>
       <div className="App-FetchedImage">
-        <FetchedImage imageInfo={fetchedImage} />
+        {/* <FetchedImage imageInfo={fetchedImage} /> */}
+        <canvas id="App-image-canvas" className="App-image-canvas-cl"></canvas>
         <NeumorphismButton onClickHandler={handleOnClick}>
           REFRESH
         </NeumorphismButton>
       </div>
-      <ColourPalette />
-      <p className="App-copyright">made for fun by <span className="App-copyright-author">william to</span></p>
+      <ColourPalette
+        colours={colours}
+      />
+      <p className="App-copyright">made for fun by <span className="App-copyright-author"><a href="https://github.com/wlto">william to</a></span></p>
     </div>
   );
 }
--- src/ColourPalette/ColourPalette.js
+++ src/ColourPalette/ColourPalette.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './ColourPalette.css';
 
 function ColourPalette(props) {
+  const { colours } = props;
+
+  const colourBoxes = colours.map((colour, index) => {
+    return (
+      <div className="ColourPalette-colour-box">
+        <div className="ColourPalette-colour-box-box" style={{
+          backgroundColor: `${colour.hexValue}`
+        }}></div>
+        <p className="ColourPalette-colour-box-hex">{colour.hexValue}</p>
+        <p className="ColourPalette-colour-box-percentage">{colour.percentage}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="ColourPalette">
       <h3 className="ColourPalette-title">DOMINANT COLOURS</h3>
-      <div className="ColourPalette-colour-container">
-        <div className="ColourPalette-colour-box">blue</div>
-        <p className="ColourPalette-colour-hex">#0000ff</p>
-        <p className="ColourPalette-colour-percentage">100%</p>
+      <div className="ColourPalette-colours-container">
+        {colourBoxes}
       </div>
     </div>
   );
